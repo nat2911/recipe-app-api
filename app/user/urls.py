57be61673ff5e7b48ded4e7740ae7b1f8562ad7c
Nandan Tendulkar <nandan.tendulkar@gmail.com>
2023-08-20 18:09:14 +0530

    [DONE] User API

--- app/user/urls.py
+++ app/user/urls.py
@@ -14,4 +14,14 @@ urlpatterns = [
         views.CreateUserView.as_view(),
         name="create",
     ),
+    path(
+        "token/",
+        views.CreateTokenView.as_view(),
+        name="token",
+    ),
+    path(
+        "me/",
+        views.ManageUserView.as_view(),
+        name="me",
+    ),
 ]
